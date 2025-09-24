2536b74955995108d352eb768eed2d0e1dd0e2a5
Niyaz-H <huseynzadehniyaz@gmail.com>
2025-09-24 16:54:46 +0200

    feat(deploy): Configure static and media file handling for production

--- afpms_project/settings.py
+++ afpms_project/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
