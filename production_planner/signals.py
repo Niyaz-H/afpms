49e6bf4fd945043f56fd4c1e7f057112bda74a3d
Niyaz-H <huseynzadehniyaz@gmail.com>
2025-09-24 17:08:21 +0200

    fix(tests): Correct unit test for inventory signal logic

--- production_planner/signals.py
+++ production_planner/signals.py
@@ -1,31 +1,39 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import ProductionBatch
-from inventory.models import Inventory
+from django.contrib.contenttypes.models import ContentType
+from .models import ProductionBatch, Product, BatchMaterial
+from inventory.models import Inventory, Material
 
 @receiver(post_save, sender=ProductionBatch)
-def update_inventory_on_batch_creation(sender, instance, created, **kwargs):
+def update_product_inventory_on_batch_creation(sender, instance, created, **kwargs):
     """
-    Updates the inventory when a new production batch is created.
+    Increases the inventory of the finished product when a new production batch is created.
     """
     if created:
-        # Decrease the stock of materials used in the batch
-        for batch_material in instance.batchmaterial_set.all():
-            material = batch_material.material
-            quantity_to_deduct = batch_material.quantity
-            
-            inventory_item, created = Inventory.objects.get_or_create(
-                content_type__model='material',
-                object_id=material.id
-            )
-            inventory_item.stock_level -= quantity_to_deduct
-            inventory_item.save()
-
-        # Increase the stock of the finished product
+        product_content_type = ContentType.objects.get_for_model(Product)
         product = instance.product
         inventory_item, created = Inventory.objects.get_or_create(
-            content_type__model='product',
-            object_id=product.id
+            content_type=product_content_type,
+            object_id=product.id,
+            defaults={'stock_level': 0}
         )
         inventory_item.stock_level += instance.quantity
+        inventory_item.save()
+
+@receiver(post_save, sender=BatchMaterial)
+def update_material_inventory_on_batch_material_creation(sender, instance, created, **kwargs):
+    """
+    Decreases the inventory of a material when it is added to a batch.
+    """
+    if created:
+        material = instance.material
+        quantity_to_deduct = instance.quantity
+        
+        material_content_type = ContentType.objects.get_for_model(Material)
+        inventory_item, created = Inventory.objects.get_or_create(
+            content_type=material_content_type,
+            object_id=material.id,
+            defaults={'stock_level': 0}
+        )
+        inventory_item.stock_level -= quantity_to_deduct
         inventory_item.save()
--- production_planner/tests.py
+++ production_planner/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from .models import Product, ProductionBatch, BatchMaterial
+from inventory.models import Material, Inventory
 
-# Create your tests here.
+class ProductionBatchModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.product = Product.objects.create(title='Test Product', target_co2_reduction=1.5)
+        self.material1 = Material.objects.create(name='Cotton', sustainable_rating=8, cost_per_unit=10.00)
+        self.material2 = Material.objects.create(name='Polyester', sustainable_rating=3, cost_per_unit=5.00)
+
+        # Create initial inventory
+        Inventory.objects.create(content_object=self.material1, stock_level=100)
+        Inventory.objects.create(content_object=self.material2, stock_level=200)
+        Inventory.objects.create(content_object=self.product, stock_level=50)
+
+    def test_inventory_updated_on_batch_creation(self):
+        """
+        Test that the post_save signal correctly updates inventory when a batch is created.
+        """
+        batch = ProductionBatch.objects.create(
+            product=self.product,
+            created_by=self.user,
+            quantity=10
+        )
+        BatchMaterial.objects.create(batch=batch, material=self.material1, quantity=20)
+        BatchMaterial.objects.create(batch=batch, material=self.material2, quantity=30)
+
+        # Check material inventory
+        material_content_type = ContentType.objects.get_for_model(Material)
+        self.assertEqual(Inventory.objects.get(content_type=material_content_type, object_id=self.material1.id).stock_level, 80)
+        self.assertEqual(Inventory.objects.get(content_type=material_content_type, object_id=self.material2.id).stock_level, 170)
+
+        # Check product inventory
+        product_content_type = ContentType.objects.get_for_model(Product)
+        self.assertEqual(Inventory.objects.get(content_type=product_content_type, object_id=self.product.id).stock_level, 60)
